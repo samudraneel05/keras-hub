1f2dcc284c817432e0522672a854f55f416f7d6b
Samudraneel Sarkar <samudraneel05@gmail.com>
2026-02-18 19:08:03 +0530

    changes to conversion method and uncommenting for testing

--- keras_hub/src/models/qwen2_vl/__init__.py
+++ keras_hub/src/models/qwen2_vl/__init__.py
@@ -1,6 +1,5 @@
 from keras_hub.src.models.qwen2_vl.qwen2_vl_backbone import Qwen2VLBackbone
+from keras_hub.src.models.qwen2_vl.qwen2_vl_presets import backbone_presets
+from keras_hub.src.utils.preset_utils import register_presets
 
-# TODO: Presets disabled until weights are uploaded to Kaggle
-# from keras_hub.src.models.qwen2_vl.qwen2_vl_presets import backbone_presets
-# from keras_hub.src.utils.preset_utils import register_presets
-# register_presets(backbone_presets, Qwen2VLBackbone)
+register_presets(backbone_presets, Qwen2VLBackbone)
